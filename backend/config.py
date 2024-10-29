6fd49b1133775bd2cc965b6f288012c95a6170fd
olympiahardy <olympia.hardy13@hotmail.co.uk>
2024-10-29 11:11:24 +0000

    UMAP working work on spatial tomorrow

--- backend/config.py
+++ backend/config.py
@@ -12,6 +12,7 @@ from vitessce import VitessceConfig, AnnDataWrapper, ViewType as vt, SpatialData
 # This initialises the application
 app = Flask(__name__)
 # This disables an error that will allow us to send cross-origin requests to our app
+print("cors")
 CORS(app)
 
 BASE_DIR = os.path.abspath("/Users/olympia/cellXplore_App/datasets/")
@@ -25,12 +26,12 @@ def create_vitessce_config(zarr_file_path, config_name, dataset_name):
     dataset = vc.add_dataset(name=dataset_name).add_object(
         AnnDataWrapper(
             zarr_file_path,
-            obs_embedding_paths=["obsm/X_umap"],          # UMAP embeddings
-            obs_embedding_names=["UMAP"],
+            obs_embedding_paths=["obsm/X_umap", "uns/spatial/Naive_1"],          # UMAP embeddings
+            obs_embedding_names=["UMAP", "spatial"],
             obs_set_paths=["obs/clusters"],               # Cluster labels
             obs_set_names=["clusters"],
             obs_feature_matrix_path="X",                  # Feature matrix
-            #obs_spots_path="obsm/spatial",                # Spatial coordinates
+            # obs_spots_path="obsm/spatial",                # Spatial coordinates
             #spatial_image_layer="hires",                  # The resolution layer to use
             #spatial_image_path="uns/spatial/Naive_1/images/hires",  # Updated path for hires image
             #spatial_image_key="Naive_1"                   # Corresponding key for the image
@@ -39,12 +40,11 @@ def create_vitessce_config(zarr_file_path, config_name, dataset_name):
     pprint("LOOOOOK HERE!!!!!")
     pprint(dataset)
 
-    #spatial_view = vc.add_view(vt.SPATIAL, dataset = dataset, mapping="spatial")
-    umap_view = vc.add_view(dataset = dataset, view_type='scatterplot', mapping="UMAP")
+    spatial_view = vc.add_view(dataset = dataset, mapping="spatial", view_type=vt.SPATIAL)
+    umap_view = vc.add_view(dataset = dataset, view_type=vt.SCATTERPLOT, mapping="UMAP")
 
     # Here we define what plots and views we want to visualise
-    #vc.layout(hconcat(spatial_view, umap_view))
-    vc.layout(umap_view)
+    vc.layout(hconcat(spatial_view, umap_view))
     # Exports the Vitessce config file to a JSON format to your API endpoint in main.py
     pprint(vc)
     return vc.to_dict(base_url='http://127.0.0.1:5000/datasets')
